e250abe15f87399180f3adb3752cf4f8f60a5112
Marco Minerva <marco.minerva@gmail.com>
2018-05-14 11:21:32 +0200

    Add AuthenticationResult

--- Src/RestLibrary/Models/AuthenticationResult.cs
+++ Src/RestLibrary/Models/AuthenticationResult.cs
@@ -11,5 +11,14 @@ namespace RestLibrary.Models
         public bool Succeeded { get; internal set; }
 
         public AuthenticationError Error { get; internal set; }
+
+        public AuthenticationResult(bool succeded = false, string error = null, string errorDescription = null)
+        {
+            Succeeded = succeded;
+            if (!string.IsNullOrWhiteSpace(error) || !string.IsNullOrWhiteSpace(errorDescription))
+            {
+                Error = new AuthenticationError(error, errorDescription);
+            }
+        }
     }
 }
